f79c451b657c768fe5407cc0d610292965be4910
seema patil <seemampatil22@gmail.com>
2025-02-12 10:41:07 +0530

    Add files via upload
    greatest of tree  number

---
+++ greatest.java
@@ -0,0 +1,16 @@
+public class greatest {
+    public static void main(String[] args) {
+        int a=100;
+        int b=20;
+        int c=30;
+        if(a>b && a>c){
+            System.out.println(a+" is greatest number");
+        }
+        else if (b>c) {
+            System.out.println(b+"is greatest number");
+        }
+        else{
+            System.out.println(c+" is greatest number");
+        }
+    }
+}
