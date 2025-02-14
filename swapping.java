943e45e44844ebab518756d125e2ee8488c2ce62
seema patil <seemampatil22@gmail.com>
2025-02-14 11:14:58 +0530

    Add files via upload
    swapping of two number program

---
+++ swapping.java
@@ -0,0 +1,12 @@
+public class swapping {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c;
+        c=b;
+        b=a;
+        a=c;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+}
