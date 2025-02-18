c67efc79523f80a22800b78c49442e945364ccd9
seema patil <seemampatil22@gmail.com>
2025-02-18 10:37:57 +0530

    Add files via upload
    checking number even or not

---
+++ Even.java
@@ -0,0 +1,13 @@
+public class Even {
+    public static void main(String[] args) {
+        int n=10;
+    
+        if(n%2==0){
+           System.out.println(n+"is even number"); 
+        }
+        else{
+            System.out.println(n+"is not even number");
+        }
+    }
+    }
+
