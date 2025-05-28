d85ac0b263bc67663ddb1ae38009e7c2c72fcb00
seema patil <seemampatil22@gmail.com>
2025-05-28 18:29:30 +0530

    Add files via upload
    Armstrong number

--- Armstrong.java
+++ Armstrong.java
@@ -1,23 +1,43 @@
+
+    import java.util.Scanner;
 public class Armstrong {
-static int countDigit(int n){
-    if (n==0) {
-      return 0;  
-    }
-    return 1+countDigit(n/10);
-}
-static boolean isAremstrong(int n,int orginal,int numberDigit){
-    if (n==0) {
-       return orginal== 0; 
-    }
-    int digit=n%10;
-    return isAremstrong(n/10,orginal-(int)Math.pow(digit, numberDigit));
-}
 
     public static void main(String[] args) {
-     int numberDigit=countDigit(n);
-     if (isAremstrong(n)) {
-        
-     }   
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
 
-   }
+        int original = num;
+        int sum = 0;
+
+        int temp = num;
+        int count = 0;
+        while (temp > 0) {
+            count++;
+            temp /= 10;
+        }
+
+        
+        temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            int power = 1;
+
+            
+            for (int i = 0; i < count; i++) {
+                power *= digit;
+            }
+
+            sum += power;
+            temp /= 10;
+        }
+
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is not an Armstrong number.");
+        }
+    }
+}
+
+
