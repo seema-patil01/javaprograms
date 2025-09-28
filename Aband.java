407bd801b7d1ecc50dbebf922ca68c15dacc72d8
seema patil <seemampatil22@gmail.com>
2025-09-28 11:48:36 +0530

    Add files via upload
    Abandoned Object Example

---
+++ Aband.java
@@ -0,0 +1,8 @@
+public class Aband {
+    int i=50;
+    public static void main(String[] args) {
+        Aband a=new Aband();
+        System.out.println(a.i);
+        System.out.println(new Aband().i);
+    }
+}
