af91d4072b45db613018247d7b716ccf35dea123
seema patil <seemampatil22@gmail.com>
2025-10-03 21:07:24 +0530

    Add files via upload
    Constructor copy example

--- Student.java
+++ Student.java
@@ -1,24 +1,28 @@
 public class Student {
-   
+    
     String name;
     int age;
-
-    // constructor using 'this' keyword example
-    Student(String name, int age) {
-        this.name = name; 
-        this.age = age;
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+    Student(Student s) {
+        name = s.name;
+        age = s.age;
     }
 
     void display() {
-        System.out.println("Name: " + this.name);
-        System.out.println("Age: " + this.age);
+        System.out.println("Name: " + name + ", Age: " + age);
     }
-}
 
-class Main {
     public static void main(String[] args) {
         Student s1 = new Student("Seema", 22);
+
+        
+        Student s2 = new Student(s1);
+
         s1.display();
+        s2.display();
     }
 }
 
