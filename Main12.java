25f2073557b3849da8d77694b7e5faf5cfa805ef
seema patil <seemampatil22@gmail.com>
2025-10-21 22:50:45 +0530

    Add files via upload
    Early Binding example

---
+++ Main12.java
@@ -0,0 +1,15 @@
+
+    class Calculator {
+    void add(int a, int b) {
+        System.out.println("Sum: " + (a + b));
+    }
+}
+
+public class Main12 {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.add(10, 20);  
+    }
+}
+
+
