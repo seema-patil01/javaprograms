e0ca3db0fc1550822177ace96ab766598230b6d3
seema patil <seemampatil22@gmail.com>
2025-10-22 21:36:33 +0530

    Add files via upload
    Late Binding Example

---
+++ Main13.java
@@ -0,0 +1,21 @@
+
+    class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main13 {
+    public static void main(String[] args) {
+        Animal a = new Dog(); 
+        a.sound();  
+    }
+}
+
+
